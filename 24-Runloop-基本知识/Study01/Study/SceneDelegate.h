7fd0f2efbee04a459b503a5fb6e6d67ceeb12b7d
孟宪亮 <mengxianliangok@163.com>
2021-08-30 10:51:05 +0800

    666

---
+++ 24-Runloop-基本知识/Study01/Study/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  Study
+//
+//  Created by 孟宪亮 on 2021/8/28.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ 24-Runloop-基本知识/Study01/Study/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Study
+//
+//  Created by 孟宪亮 on 2021/8/28.
+//
+
+#import "ViewController.h"
+
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    [NSRunLoop currentRunLoop];
+    
+    CFRunLoopGetCurrent();
+    
+}
+
+@end
