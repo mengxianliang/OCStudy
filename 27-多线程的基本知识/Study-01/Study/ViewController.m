235bf7de6a45df5f784b382fa2b174df5805f43e
孟宪亮 <mengxianliangok@163.com>
2021-09-01 16:59:21 +0800

    666

--- 27-多线程的基本知识/Study-01/Study/ViewController.m
+++ 27-多线程的基本知识/Study-01/Study/ViewController.m
@@ -16,7 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [self test5];
+    [self test7];
 }
 
 // 任务：同步、异步
@@ -109,6 +109,23 @@
         });
         
         NSLog(@"4");
+    });
+    
+    NSLog(@"5");
+}
+
+- (void)test7 {
+    NSLog(@"1");
+    
+    dispatch_queue_t queue = dispatch_queue_create("myQueue", DISPATCH_QUEUE_CONCURRENT);
+    dispatch_async(queue, ^{
+        NSLog(@"2 %@", [NSThread currentThread]);
+        
+        dispatch_sync(queue, ^{
+            NSLog(@"3 %@", [NSThread currentThread]);
+        });
+        
+        NSLog(@"4 %@", [NSThread currentThread]);
     });
     
     NSLog(@"5");
