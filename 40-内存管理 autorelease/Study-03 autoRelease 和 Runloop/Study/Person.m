90d41ea7d5fbfdacab3843e6812ad018f181d6d2
孟宪亮 <mengxianliangok@163.com>
2021-09-10 11:50:51 +0800

    666

---
+++ 40-内存管理 autorelease/Study-03 autoRelease 和 Runloop/Study/Person.m
@@ -0,0 +1,17 @@
+//
+//  Person.m
+//  Study
+//
+//  Created by mxl on 2021/9/10.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+- (void)dealloc {
+    NSLog(@"%s", __func__);
+    [super dealloc];
+}
+
+@end
---
+++ 40-内存管理 autorelease/Study-03 autoRelease 和 Runloop/Study/ViewController.m
@@ -0,0 +1,38 @@
+//
+//  ViewController.m
+//  Study
+//
+//  Created by mxl on 2021/8/24.
+//
+
+#import "ViewController.h"
+#import "Person.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    Person *person = [[[Person alloc] init] autorelease];
+     
+    NSLog(@"%s", __func__);
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    
+    NSLog(@"%s", __func__);
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    
+    NSLog(@"%s", __func__);
+}
+
+
+@end
